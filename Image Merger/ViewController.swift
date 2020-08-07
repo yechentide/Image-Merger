2fe1471489fcddfc9faafaddc81c502b0ded24b5
yechentide <yechentide@gmail.com>
2020-08-07 22:13:54 +0900

    create project

---
+++ Image Merger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Merger
+//
+//  Created by yechen on 2020/08/07.
+//  Copyright © 2020 Polarnight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
