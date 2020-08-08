d25b6fb7ff150a642431a5a90bfbe1ff2e6174ef
yechentide <yechentide@gmail.com>
2020-08-08 17:45:39 +0900

    added the function to read photos from camera roll

--- Image Merger/ImageCell.swift
+++ Image Merger/ImageCell.swift
@@ -18,7 +18,44 @@ class ImageCell: UICollectionViewCell {
         // Initialization code
     }
 
-    func setup(img: UIImage) {
+    func setImage(name: String) {
+        imageView.image = UIImage(named: name)!
+        imageView.image?.accessibilityIdentifier = name
+    }
+    
+    func setImage(img: UIImage) {
         imageView.image = img
+    }
+    
+    
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
     }
 }
--- Image Merger/ViewController.swift
+++ Image Merger/ViewController.swift
@@ -11,15 +11,22 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var editBtn: UIBarButtonItem!
+    @IBOutlet weak var mergeBtn: UIBarButtonItem!
+    
+    var list = [UIImage]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        collectionView.register(UINib(nibName: "ImageCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        list.append(UIImage(named: "add")!)
         
         collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: "ImageCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        
         
         let w = UIScreen.main.bounds.width
         let l = (w-24-24-30)/2
@@ -30,6 +37,8 @@ class ViewController: UIViewController {
         layout.minimumInteritemSpacing = 30
         layout.minimumLineSpacing = 30
         collectionView.collectionViewLayout = layout
+        
+        
     }
 
     
@@ -40,19 +49,60 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return list.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         
         if let cell = cell as? ImageCell {
-            cell.setup(img: UIImage(named: "add")!)
+            cell.setImage(img: list[indexPath.row])
         }
         
         return cell
     }
     
+}
+
+extension ViewController: UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected: \(indexPath.row)")
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        // picker.mediaTypes = ["public.image", "public.movie"]
+        self.present(picker, animated: true, completion: nil)
+        
+    }
     
+}
+
+
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // 画像が選択された時に呼ばれる
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])  {
+        let indexPaths = collectionView.indexPathsForSelectedItems
+        if let indexPath = indexPaths {
+            if let selectedImage = info[.originalImage] as? UIImage {
+                
+                
+                if indexPath[0].row==list.count-1 {
+                    list.append(UIImage(named: "add")!)
+                }
+                list[indexPath[0].row] = selectedImage
+                collectionView.reloadData()
+                
+                
+            }
+        }
+        self.dismiss(animated: true)  //画像をImageViewに表示したらアルバムを閉じる
+    }
+    
+    // 画像選択がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
